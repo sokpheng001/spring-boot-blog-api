85ac2e8157f4e8e74052be609e43e2c8156527a8
sokpheng001 <kimchansokpheng123@gmail.com>
2026-02-19 16:54:00 +0700

    update: updated shell script file

--- push.sh
+++ push.sh
@@ -3,7 +3,6 @@ read -p "[+] Commit message: " msg;
 git commit -m "$msg";
 echo -m "Current branch: "
 git branch
-echo "Are you sure to push?..."
-read
+echo -n "Are you sure to push? ..." read
 git push -u origin main 
 
